1daef8bca44c9c9688a5d1ede17f4ae5c9a8cf30
kooku <koomg9599@gmail.com>
2021-01-17 22:26:44 +0900

    second commit

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -1,20 +1,40 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import Img from 'gatsby-image'
+import React from "react";
+import { Link } from "gatsby";
+import Img from "gatsby-image";
 
-import styles from './article-preview.module.css'
+import styles from "./article-preview.module.css";
 
 export default ({ article }) => (
-  <div className={styles.preview}>
-    <Img alt="" fluid={article.heroImage.fluid} />
-    <h3 className={styles.previewTitle}>
-      <Link to={`/blog/${article.slug}`}>{article.title}</Link>
-    </h3>
-    <small>{article.publishDate}</small>
-    <p
-      dangerouslySetInnerHTML={{
-        __html: article.description.childMarkdownRemark.html,
-      }}
-    />
+  <div>
+    <Link to={`/blog/${article.slug}`}>
+      <div className={styles.preview}>
+        <div style={{ width: 180, height: 100, marginRight: 20 }}>
+          <Img
+            className={styles.previewImg}
+            alt=""
+            fluid={article.heroImage.fluid}
+            objectFit="cover"
+            objectPosition="50% 50%"
+          />
+        </div>
+        <div className={styles.previewContents}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "flex-end",
+            }}
+          >
+            <h3 className={styles.previewTitle}>{article.title}</h3>
+            <small className={styles.publishDate}>{article.publishDate}</small>
+          </div>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: article.description.childMarkdownRemark.html,
+            }}
+          />
+        </div>
+      </div>
+    </Link>
   </div>
-)
+);
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,23 @@
-import React from 'react'
-import './base.css'
-import Container from './container'
-import Navigation from './navigation'
+import React from "react";
+import "./base.css";
+import Container from "./container";
 
 class Template extends React.Component {
   render() {
-    const { children } = this.props
+    const { children } = this.props;
 
     return (
       <Container>
-        <Navigation />
+        <h1 style={{ fontWeight: 800, fontSize: 54, marginBottom: 10 }}>
+          KASA Blog
+        </h1>
+        <div style={{ marginBottom: 20, fontSize: 24 }}>
+          <b style={{ color: "#1d6ff2" }}>카사 블로그</b> 데모
+        </div>
         {children}
       </Container>
-    )
+    );
   }
 }
 
-export default Template
+export default Template;
